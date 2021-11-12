a3d9acdc8c30b9afa1459fc9c6425931c595b872
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-11-12 15:49:26 +0530

    Update GasflipDDVR.sol

--- GasflipDDVR.sol
+++ GasflipDDVR.sol
@@ -1,8 +1,8 @@
 // SPDX License Indentifier: MIT License
 
-import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
-import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "./Context.sol";
+import "./IERC20.sol";
+import "./ERC20.sol";
 
 
 pragma solidity ^0.8.3;
